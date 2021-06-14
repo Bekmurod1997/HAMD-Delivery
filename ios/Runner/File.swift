465af951cb1d10ee66042a2385c282ebe5c7376c
Bekmurod1997 <joraboyev.97@mail.ru>
2021-06-14 14:24:43 +0500

    update 14june

---
+++ ios/Runner/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Runner
+//
+//  Created by Bekmurod Juraboev on 07/06/21.
+//
+
+import Foundation
---
+++ ios/Runner/dummy.m
@@ -0,0 +1,8 @@
+//
+//  dummy.m
+//  Runner
+//
+//  Created by Bekmurod Juraboev on 07/06/21.
+//
+
+#import <Foundation/Foundation.h>
